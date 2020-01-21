ec5265fdd79b9bcbe7720bd0e9d2740c9bbdce88
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-21 11:13:08 +0100

    wip

---
+++ data/db-config.js
@@ -0,0 +1,6 @@
+// this is the file that has knowledge of knex
+// this is the file where we bring in a configuration
+// this is the file where we expose "db" to the outer world
+// "db('fruits').insert()..."
+const knex = require('knex')
+const configuration = require('../knexfile').development
