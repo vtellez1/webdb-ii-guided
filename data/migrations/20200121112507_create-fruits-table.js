a1da524e5c09b6f7e4a1eb1945c2ef6f18c583bc
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-21 11:43:36 +0100

    wip

--- data/migrations/20200121112507_create-fruits-table.js
+++ data/migrations/20200121112507_create-fruits-table.js
@@ -3,9 +3,13 @@ exports.up = function(knex) {
   // create the fruits table
   return knex.schema.createTable('fruits', table => {
     table.increments() // create column named "id", primary key, auto-incrementing
+    table.text('name', 128).unique().notNullable()
+    table.decimal('avgWeightOz').notNullable()
+    table.boolean('delicious')
   })
 };
 
 exports.down = function(knex) {
   // destroy the fruits table if it exists
+  return knex.schema.dropTableIfExists('fruits')
 };
